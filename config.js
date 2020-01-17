9158b4eec753e84c8484b33a82e89f05d7470f3a
xy4beanbag <hvhv@me.com>
2020-01-17 11:55:38 +0800

    更新

---
+++ config.js
@@ -0,0 +1,18 @@
+const headless = true;
+const cookies = [
+    
+];
+// 间隔执行时间单位分钟
+const minute = 2;
+// 上传最大百分比；
+const process = 10;
+// 上传文件名
+const fileName = "test.mp4";
+
+module.exports = {
+    headless: headless,
+    cookies: cookies,
+    minute: minute,
+    fileName: fileName,
+    process: process
+}
